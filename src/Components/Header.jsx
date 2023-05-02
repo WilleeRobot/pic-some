d41d8a8105819713125a1f0ab41d149248ae6d25
Will Lee <will@willlee.ca>
2023-05-02 15:32:33 -0600

    Add dynamic cart icon in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,20 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context";
 
 function Header() {
+  const { cartItems } = useContext(Context);
   return (
     <header>
       <Link to="/">
         <h2>Pic Some</h2>
       </Link>
       <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        {cartItems.length > 0 ? (
+          <i className="ri-shopping-cart-fill ri-fw ri-2x" />
+        ) : (
+          <i className="ri-shopping-cart-line ri-fw ri-2x" />
+        )}
       </Link>
     </header>
   );
